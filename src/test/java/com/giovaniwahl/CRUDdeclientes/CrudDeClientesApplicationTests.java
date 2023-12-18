c76733cff5ae286d563ba28c188ef6bf962e4e08
wahl <giovaniwahl@gmail.com>
2023-12-18 10:06:46 -0300

    inicio do projeto

---
+++ src/test/java/com/giovaniwahl/CRUDdeclientes/CrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovaniwahl.CRUDdeclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
