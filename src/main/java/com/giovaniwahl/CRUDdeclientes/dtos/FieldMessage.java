f53113823e46053fd02ef8fed98228e5fcbdb502
wahl <giovaniwahl@gmail.com>
2023-12-19 11:14:19 -0300

    retorno responsivo de cadastro no localizado(id)

---
+++ src/main/java/com/giovaniwahl/CRUDdeclientes/dtos/FieldMessage.java
@@ -0,0 +1,14 @@
+package com.giovaniwahl.CRUDdeclientes.dtos;
+
+public class FieldMessage {
+    private String fieldName;
+    private String message;
+
+    public FieldMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {return fieldName;}
+    public String getMessage() {return message;}
+}
