a25c6e5cb885a4d60f4857bcfd59242187a3b135
wahl <giovaniwahl@gmail.com>
2023-12-19 09:33:21 -0300

    implementado o crud no service

--- src/main/java/com/giovaniwahl/CRUDdeclientes/services/ClientService.java
+++ src/main/java/com/giovaniwahl/CRUDdeclientes/services/ClientService.java
@@ -4,7 +4,10 @@ import com.giovaniwahl.CRUDdeclientes.dtos.ClientDTO;
 import com.giovaniwahl.CRUDdeclientes.entities.Client;
 import com.giovaniwahl.CRUDdeclientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
@@ -14,8 +17,52 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Client client = clientRepository.findById(id).orElseThrow();
         return new ClientDTO(client);
+    }
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> clients = clientRepository.findAll(pageable);
+        return clients.map(ClientDTO::new);
+    }
+    @Transactional
+    public ClientDTO insert(ClientDTO clientDTO){
+       Client client = new Client();
+       copyDtoToEntity(clientDTO,client);
+       client =clientRepository.save(client);
+       return new ClientDTO(client);
+
+    }
+    @Transactional
+    public ClientDTO update(Long id,ClientDTO clientDTO){
+        try {
+            Client client = clientRepository.getReferenceById(id);
+            copyDtoToEntity(clientDTO,client);
+            client = clientRepository.save(client);
+            return new ClientDTO(client)
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Resource not found!");
+        }
+    }
+    public void delete(long id){
+        if (!clientRepository.existsById(id)){
+            throw new ResourceNotFoundException("Resource not found !");
+        }
+        try {
+            clientRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Referential integrity failure !");
+        }
+    }
+    private void copyDtoToEntity(ClientDTO clientDTO, Client client){
+        client.setName(clientDTO.getName());
+        client.setCpf(clientDTO.getCpf());
+        client.setIncome(clientDTO.getIncome());
+        client.setBirthDate(clientDTO.getBirthDate());
+        client.setChildren(clientDTO.getChildren());
     }
 }
